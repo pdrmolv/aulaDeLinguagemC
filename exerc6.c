39349b7071232e24f0ed2954418ee69a82c86afa
pdrmolv <pedromigueldeoliveira99@gmail.com>
2026-05-09 20:59:18 -0300

    Add files via upload

---
+++ exerc6.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+    int main(){
+
+        float x, z, c; 
+
+        printf("Lado 1: ");
+        scanf("%f", &x);
+
+        printf("Lado 2: ");
+        scanf("%f", &z);
+
+        printf("Lado 3: ");
+        scanf("%f", &c);
+
+        if ((x <= 0) & (z <= 0) & (c <= 0)){
+            printf("Não é possivel formar um triângulo!");
+            return 0;
+        } 
+
+        if ((x == z) & (x == c) & (z == c)){
+            printf("Triângulo Equilátero");
+        }
+        
+        else if ((x != z) & (x != c) & (z != c)){
+            printf("Triângulo Escaleno");
+        }
+        
+        else{
+            printf("Triângulo Isósceles");
+        }
+
+             // nao sei como fazer quando, por exemplo "0, 10, -1". Eu sei que n da pra montar
+                                                                // um triangulo com valores negativos.
+return 0;   }
