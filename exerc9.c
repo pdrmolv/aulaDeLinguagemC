0cb7e074d6def76ce6c2ea47c3b2879601c39c83
pdrmolv <pedromigueldeoliveira99@gmail.com>
2026-05-12 17:07:54 -0300

    Add files via upload

---
+++ exerc9.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+    int main (){
+
+        printf("Em que turno você estuda?\n");
+
+        printf("M - Matutino\n");
+        printf("V - Vespertino\n");
+        printf("N - Noturno\n");
+
+            char horario, M, V, N;
+
+        printf("Resposta: ");
+        scanf("%c", &horario);
+
+        switch (horario){
+        case 1: if (horario == M) printf("Bom dia!");
+            break;
+        case 2: if (horario == V) printf("Boa tarde!");
+            break;
+        case 3: if (horario == N) printf("Boa noite!");
+            break;
+        
+        default: printf("Invalido!");
+            break;
+        }
+
+
+
+        return 0;
+    }
