cc818a7ee7ee3f4bb7e707830aeb8dbb568a4668
pdrmolv <pedromigueldeoliveira99@gmail.com>
2026-05-15 20:22:19 -0300

    Add files via upload

---
+++ exerc9.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+    int main(){
+
+        
+        char turno;
+
+        printf("M: Matutino\n");
+        printf("V: Vespertino\n");
+        printf("N: Noturno\n");
+        
+        printf("Em que turno voçê estuda?\n");
+        scanf ("%c", &turno);
+
+        if (turno == 'M'){
+            printf("Bom dia!");
+        }
+        else if (turno == 'V'){
+            printf("Boa tarde!");
+        }
+        else if (turno == 'N'){
+            printf("Boa noite!");
+        }
+        else{
+            printf("Valor Inválido!");
+        }
+        
+        
+
+        return 0;
+    }
