cbce2c340c1afd2d3e4e555d19a19c6ef34914ce
pdrmolv <pedromigueldeoliveira99@gmail.com>
2026-05-15 20:41:32 -0300

    Fix formatting and indentation in exerc2.c

--- exerc2.c
+++ exerc2.c
@@ -8,16 +8,20 @@
         printf("Consumo em m³: ");
         scanf("%f", &consumo);
 
-        if (consumo <= 10){
+    if (consumo <= 10){
         valor = consumo = 22.38;
         printf("Valor = %.2f", valor);
-    } else if (consumo <= 20){
+    } 
+    
+    else if (consumo <= 20){
         valor = consumo * 3.50;
         printf("Valor = %.2f", valor);
-    } else if (consumo <= 50){
+    }
+     else if (consumo <= 50){
         valor = consumo * 8.75;
         printf("Valor = %.2f", valor);
-    } else {
+    } 
+     else {
         valor = consumo * 9.64;
         printf("Valor = %.2f", valor);
     }
